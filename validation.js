70380144271cee35fd6525bf8b69a9f8b99dcd02
Allan <allanchiang24@gmail.com>
2022-07-23 18:21:19 +1200

    Add javascript for regular expressions

--- validation.js
+++ validation.js
@@ -1,1 +1,12 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    telephone: /^\d{10}$/;
+};
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        console.log(e);
+    });
+});
