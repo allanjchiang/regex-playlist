fb033e2d069126099cf33f8cf14a432ef2c81afe
Allan <allanchiang24@gmail.com>
2022-07-24 16:26:26 +1200

    Add brackets around last part of email regex

--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ const inputs = document.querySelectorAll('input');
 
 const patterns = {
     username: /^[A-Za-z0-9]{5,12}$/,
-    email: /^[a-z\d\.-]+@[a-z\d-]+\.[a-z]{2,3}\.?[a-z]{0,3}?$/,
+    email: /^[a-z\d\.-]+@[a-z\d-]+\.[a-z]{2,3}(\.[a-z]{2,8})?$/,
     password: /^[\w@-]{8,20}$/,
     telephone: /^\d{10}$/,
     slug: /^[a-z\d-]{8,20}$/
